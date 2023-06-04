61bf779816c192f51122039cf106565a8b6b12bb
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2023-06-04 16:10:21 +0530

    Pyinstaller build working

---
+++ post_build.py
@@ -0,0 +1,15 @@
+# make necessary copies to dist folder
+import os
+import shutil
+
+# Copy invoice_data.toml to dist/invoice_data.toml
+shutil.copy2('invoice_data.toml', 'dist/invoice_data.toml')
+
+# Copy templates/template_01.css to dist/template_01.css
+shutil.copy2('templates/template_01.css', 'dist/template_01.css')
+
+# Copy templates/template_01.html to dist/template_01.html
+shutil.copy2('templates/template_01.html', 'dist/template_01.html')
+
+# Create output folder in dist
+os.mkdir('dist//output')
